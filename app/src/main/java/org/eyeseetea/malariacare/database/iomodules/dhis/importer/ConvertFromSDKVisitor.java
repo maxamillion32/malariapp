49596201d57114c2ec1d65701056be09cb41f4fb
ifoche <nacho.foche@gmail.com>
2016-05-01 13:21:12 +0200

    added Media attributes recognizion on pull

--- app/src/main/java/org/eyeseetea/malariacare/database/iomodules/dhis/importer/ConvertFromSDKVisitor.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/iomodules/dhis/importer/ConvertFromSDKVisitor.java
@@ -439,6 +439,7 @@ public class ConvertFromSDKVisitor implements IConvertFromSDKVisitor {
         appQuestion.setHeader(questionBuilder.saveHeader(dataElementExtended,tabGroupBuilder));
         questionBuilder.registerParentChildRelations(dataElementExtended);
         appQuestion.save();
+        questionBuilder.attachMedia(dataElementExtended, appQuestion);
         questionBuilder.add(appQuestion);
         return appQuestion;
     }
